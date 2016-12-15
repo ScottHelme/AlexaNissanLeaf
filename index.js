7110973956ebf3995d05d5024bf3a5f770605af8
Scott Helme <scotthelme@hotmail.com>
2016-12-15 15:54:14 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,151 @@
+"use strict";
+
+// Require the leaf.js file with specific vehicle functions.
+let car = require("./leaf");
+
+// Build a response to send back to Alexa.
+function buildResponse(output, card, shouldEndSession) {
+	return {
+		version: "1.0",
+		response: {
+			outputSpeech: {
+				type: "PlainText",
+				text: output,
+			},
+			card,
+			shouldEndSession
+		}
+	};
+}
+
+// Helper to build the text response for range/battery status.
+function buildBatteryStatus(battery) {
+	console.log(battery);
+	let response = `You have ${Math.floor((battery.BatteryStatusRecords.BatteryStatus.BatteryRemainingAmount / battery.BatteryStatusRecords.BatteryStatus.BatteryCapacity) * 100)}% battery which will get you approximately ${Math.floor(battery.BatteryStatusRecords.CruisingRangeAcOn * 0.000568182)} miles. `;
+
+	if (battery.BatteryStatusRecords.PluginState == "CONNECTED") {
+		response += "The car is plugged in";
+	} else {
+		response += "The car is not plugged in";
+	}
+
+	if (battery.BatteryStatusRecords.BatteryStatus.BatteryChargingStatus != "NOT_CHARGING") {
+		response += " and charging";
+	}
+
+	return response + ".";
+}
+
+// Helper to build the text response for charging status.
+function buildChargingStatus(charging) {
+	let response = "";
+	if(charging.BatteryStatusRecords.BatteryStatus.BatteryChargingStatus == "NOT_CHARGING") {
+		response += "Your car is not on charge.";
+	} else {
+		response += "Your car is on charge.";
+	}
+	
+	return response;
+}
+
+// Helper to build the text response for connected to power status.
+function buildConnectedStatus(connected) {
+	let response = "";
+	if(connected.BatteryStatusRecords.PluginState == "NOT_CONNECTED") {
+		response += "Your car is not connected to a charger.";
+	} else {
+		response += "Your car is connected to a charger.";
+	}
+	
+	return response;
+}
+
+// Handling incoming requests
+exports.handler = (event, context) => {
+	try {
+		// Verify the person calling the script. Get your Alexa Application ID here: https://developer.amazon.com/edw/home.html#/skills/list
+		// Click on the skill and look for the "Application ID" field.
+		// Set the applicationId as an environment variable or hard code it here.
+		if(event.session.application.applicationId !== process.env.applicationId) {
+			sendResponse("Invalid Application ID", "You are not allowed to use this service.");
+			return;
+		}
+		
+		// Helper to return a response with a card.		
+		const sendResponse = (title, text) => {
+			context.succeed(buildResponse(text, {
+				"type": "Simple",
+				"title": title,
+				"content": text
+			}));
+		};
+
+		// Shared callbacks.
+		const exitCallback = () => context.succeed(buildResponse("Goodbye!"));
+		const helpCallback = () => context.succeed(buildResponse("What would you like to do? You can preheat the car or ask for battery status.", null, false));
+		const loginFailureCallback = () => sendResponse("Authorisation Failure", "Unable to login to Nissan Services, please check your login credentials.");
+
+		// Handle launches without intents by just asking what to do.		
+		if (event.request.type === "LaunchRequest") {
+			helpCallback();
+		} else if (event.request.type === "IntentRequest") {
+			// Handle different intents by sending commands to the API and providing callbacks.
+			switch (event.request.intent.name) {
+				case "PreheatIntent":
+					car.sendPreheatCommand(
+						response => sendResponse("Car Preheat", "The car is warming up for you."),
+						() => sendResponse("Car Preheat", "I can't communicate with the car at the moment.")
+					);
+					break;
+				case "CoolingIntent":
+					car.sendCoolingCommand(
+						response => sendResponse("Car Cooling", "The car is cooling down for you."),
+						() => sendResponse("Car Cooling", "I can't communicate with the car at the moment.")
+					);
+					break;
+				case "UpdateIntent":
+					car.sendUpdateCommand(
+						response => sendResponse("Car Update", "I'm downloading the latest data for you."),
+						() => sendResponse("Car Update", "I can't communicate with the car at the moment.")
+					);
+					break;
+				case "RangeIntent":
+					car.getBatteryStatus(
+						response => sendResponse("Car Range Status", buildBatteryStatus(response)),
+						() => sendResponse("Car Range Status", "Unable to get car battery status.")
+					);
+					break;
+				case "ChargeIntent":
+					car.getBatteryStatus(
+						response => sendResponse("Car Battery Status", buildBatteryStatus(response)),
+						() => sendResponse("Car Battery Status", "Unable to get car battery status.")
+					);
+					break;
+				case "ChargingIntent":
+					car.getBatteryStatus(
+						response => sendResponse("Car Charging Status", buildChargingStatus(response)),
+						() => sendResponse("Car Charging Status", "Unable to get car battery status.")
+					);
+					break;
+				case "ConnectedIntent":
+					car.getBatteryStatus(
+						response => sendResponse("Car Connected Status", buildConnectedStatus(response)),
+						() => sendResponse("Car Connected Status", "Unable to get car battery status.")
+					);
+					break;
+				case "AMAZON.HelpIntent":
+					helpCallback();
+					break;
+				case "AMAZON.StopIntent":
+				case "AMAZON.CancelIntent":
+					exitCallback();
+					break;
+			}
+		} else if (event.request.type === "SessionEndedRequest") {
+			exitCallback();
+		}
+	} catch (err) {
+		console.error(err.message);
+		sendResponse("Error Occurred", "An error occurred. Fire the programmer! " + err.message);
+	}
+};
---
+++ leaf.js
@@ -0,0 +1,131 @@
+"use strict";
+
+let https = require("https");
+
+// Do not change this value, it is static.
+let initial_app_strings = "geORNtsZe5I4lRGjG9GZiA";
+// Possible value are NE (Europe), NNA (North America) and NCI (Canada).
+let region_code = "NE";
+// You should store your username and password as environment variables. 
+// If you don't you can hard code them in the following variables.
+let username = process.env.username; // Your NissanConnect username or email address.
+let password = process.env.password; // Your NissanConnect account password.
+
+let sessionid, vin, loginFailureCallback;
+
+/**
+* Sends a request to the Nissan API.
+*
+* action - The API endpoint to call, like UserLoginRequest.php.
+* requestData - The URL encoded parameter string for the current call.
+* successCallback
+* failureCallback
+**/
+function sendRequest(action, requestData, successCallback, failureCallback) {	
+	const options = {
+		hostname: "gdcportalgw.its-mo.com",
+		port: 443,
+		path: "/gworchest_160803EC/gdc/" + action,
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			"Content-Length": Buffer.byteLength(requestData),
+		}
+	};
+
+	const req = https.request(options, resp => {
+		if (resp.statusCode < 200 || resp.statusCode > 300) {
+			console.log(`Failed to send request ${action} (${resp.statusCode}: ${resp.statusMessage})`);
+			if (failureCallback)
+				failureCallback();
+			return;
+		}
+
+		console.log(`Successful request ${action} (${resp.statusCode}: ${resp.statusMessage})`);
+		let respData = "";
+
+		resp.on("data", c => {
+			respData += c.toString();
+		});
+		resp.on("end", () => {
+			let json = respData && respData.length ? JSON.parse(respData) : null;
+			if (json.status == 200) {
+				successCallback(respData && respData.length ? JSON.parse(respData) : null);
+			}
+		});
+	});
+	
+	req.write(requestData);
+	req.end();
+}
+
+/**
+* Log the current user in to retrieve a valid session token.
+* 
+* successCallback
+**/
+function login(successCallback) {
+	sendRequest("UserLoginRequest.php", 
+	"UserId=" + username +
+	"&initial_app_strings=" + initial_app_strings +
+	"&RegionCode=" + region_code +
+	"&Password=" + password,
+	loginResponse => {
+		// Get the session id and VIN for future API calls.
+		sessionid = encodeURIComponent(loginResponse.vehicleInfo[0].custom_sessionid);
+		vin = encodeURIComponent(loginResponse.vehicleInfo[0].vin);
+		successCallback();
+	}, 
+	loginFailureCallback);
+}
+
+/**
+* Get the battery information from the API.
+**/
+exports.getBatteryStatus = (successCallback, failureCallback) => {
+	login(() => sendRequest("BatteryStatusRecordsRequest.php",
+	"custom_sessionid=" + sessionid +
+	"&RegionCode=" + region_code +
+	"&VIN=" + vin,
+	successCallback,
+	failureCallback));
+}
+
+/**
+* Enable the climate control in the car.
+**/
+exports.sendPreheatCommand = (successCallback, failureCallback) => {
+	login(() => sendRequest("ACRemoteRequest.php",
+	"UserId=" + username +
+	"&custom_sessionid=" + sessionid +
+	"&RegionCode=" + region_code +
+	"&VIN=" + vin,
+	successCallback,
+	failureCallback));
+}
+
+/**
+* Enable the climate control in the car.
+**/
+exports.sendCoolingCommand = (successCallback, failureCallback) => {
+	login(() => sendRequest("ACRemoteRequest.php",
+	"UserId=" + username +
+	"&custom_sessionid=" + sessionid +
+	"&RegionCode=" + region_code +
+	"&VIN=" + vin,
+	successCallback,
+	failureCallback));
+}
+
+/**
+* Request the API fetch updated data from the car.
+**/
+exports.sendUpdateCommand = (successCallback, failureCallback) => {
+	login(() => sendRequest("BatteryStatusCheckRequest.php",
+	"UserId=" + username +
+	"&custom_sessionid=" + sessionid +
+	"&RegionCode=" + region_code +
+	"&VIN=" + vin,
+	successCallback,
+	failureCallback));
+}
